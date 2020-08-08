912997de7f89782057be53bec9cbcfbd07b3d832
Niceguyfmk <faizmdkhan@hotmail.com>
2020-08-08 21:19:42 +0300

    CHANGED TO CLASSES THAN IDS

--- videos.js
+++ videos.js
@@ -1,25 +1,36 @@
 // Get the modal
-var modal = document.getElementById("myModal");
+var modal = document.getElementsByClassName("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var img = document.getElementsByClassName("myImg");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+//Pause video
+var p = document.getElementsById("myVideo");
+
+function pauseVid() {
+  p.pause();
 }
+
+//Set Volume of Video
+var vol = document.getElementById("myVideo").volume = 0.1;
+
+// When the user clicks on the button, open the modal
+img.onclick = function() {
+    modal.style.display = "block";
+  }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+// When the user clicks anywhere outside of the modal, close it & pause the video
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    p.pause();
   }
 }
